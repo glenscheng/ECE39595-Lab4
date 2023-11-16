4d6b26222cca2334cf915fae9817bb7f449df8c4
Glen Shaofu Cheng <gscheng@eceprog4.ecn.purdue.edu>
2023-11-16 13:20:13 -0500

    reverting back to normal multiplication and removed a print statement

--- temp.cpp
+++ temp.cpp
@@ -59,7 +59,7 @@
     }**/
 
 
-/* WIP- THREADING FOR MULTIPLICATION
+/* 231115 WIP- THREADING FOR MULTIPLICATION
 void multiply_term(power curr_power, coeff curr_coeff, const polynomial other, polynomial &temp, int k) {
   auto other_begin = other.get_poly().begin();
   auto other_end = other.get_poly().end();
@@ -95,6 +95,53 @@ polynomial polynomial::operator*(const polynomial& other) const {
     threads.push_back(thread(multiply_term, iter->first, iter->second, other_copy, ref(temps.at(i)), k));
     threads.at(i);
     i++;
+  }
+
+  // wait for threads to finish
+  for (int i = 0; i < size; i++) {
+    threads.at(i).join();
+  }
+
+  // sum temps for result
+  polynomial result;
+  for (polynomial p : temps) {
+    result = result + p;
+  }
+
+  return result;
+}
+*/
+
+/* 231116 WIP- THREADING FOR MULTIPLICATION: works, gotta find out if it is faster or not, also account for only 128 threads
+static void multiply_terms(power this_power, coeff this_coeff, power other_power, coeff other_coeff, polynomial &temp) {
+  temp.insert_poly(this_power + other_power, this_coeff * other_coeff);
+}
+
+// Multiplies two polynomials (polynomial * polynomial) and returns the result
+polynomial polynomial::operator*(const polynomial& other) const {
+  polynomial zero;
+  int size = (*this).poly.size() * other.poly.size();
+  vector<polynomial> temps(size, zero); // initialize temps vector to all 0 polynomials
+
+  // int k = 1;
+  
+  // initialize threads vector AND call threads
+  vector<thread> threads;
+  int i = 0;
+  auto this_end = (*this).poly.rend();
+  auto other_end = other.poly.rend();
+  for (auto this_iter = (*this).poly.rbegin(); this_iter != this_end; this_iter++) {
+    for (auto other_iter = other.poly.rbegin(); other_iter != other_end; other_iter++) {
+      power this_power = (*this_iter).first;
+      coeff this_coeff = (*this_iter).second;
+      power other_power = (*other_iter).first;
+      coeff other_coeff = (*other_iter).second;
+      // cout << "(before calling threads) this: " << this_coeff << "x^" << this_power << ", other: " << other_coeff << "x^" << other_power << endl;
+      threads.push_back(thread(multiply_terms, this_power, this_coeff, other_power, other_coeff, ref(temps.at(i))));
+      //threads.push_back(thread(multiply_terms, this_power, this_coeff, other_power, other_coeff, ref(result)));
+      threads.at(i);
+      i++;
+    }
   }
 
   // wait for threads to finish
