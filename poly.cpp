769708e6d00948dea9fcd7c853456669152bd161
Glen Shaofu Cheng <gscheng@eceprog4.ecn.purdue.edu>
2023-11-16 13:17:46 -0500

    made threading work, need to check if it is faster, also need to account for only 128 threads

--- poly.cpp
+++ poly.cpp
@@ -203,48 +203,36 @@ polynomial operator*(const int i, const polynomial& polynomial_object) {
   return result;
 }
 
-void multiply_term(power curr_power, coeff curr_coeff, const polynomial other, polynomial &temp, int k) {
-  auto other_begin = other.get_poly().begin();
-  auto other_end = other.get_poly().end();
-  cout << "this curr: " << curr_coeff << "x^" << curr_power << endl;
-  while (other_begin != other_end) {
-    temp.insert_poly(curr_power + (*other_begin).first, curr_coeff * (*other_begin).second);
-    cout << "other curr: " << (*other_begin).second << "x^" << (*other_begin).first << endl;
-    cout << "temp:";
-    temp.print();
-    other_begin++;
-  }
-  cout << endl;
+
+static void multiply_terms(power this_power, coeff this_coeff, power other_power, coeff other_coeff, polynomial &temp) {
+  temp.insert_poly(this_power + other_power, this_coeff * other_coeff);
 }
 
 // Multiplies two polynomials (polynomial * polynomial) and returns the result
 polynomial polynomial::operator*(const polynomial& other) const {
   polynomial zero;
-  int size = this->poly.size();
-  vector<polynomial> temps(size);
+  int size = (*this).poly.size() * other.poly.size();
+  vector<polynomial> temps(size, zero); // initialize temps vector to all 0 polynomials
 
-  // initialize temps vector
-  for (int i = 0; i < size; i++) {
-    temps.at(i) = zero;
-  }
-
-  int k = 1;
-
-  // initialize threads vector 
+  // int k = 1;
+  
+  // initialize threads vector AND call threads
   vector<thread> threads;
   int i = 0;
-  for (auto iter = (this -> poly).rbegin(); iter != (this -> poly).rend(); iter++) {
-    polynomial other_copy = other;
-    auto curr_power_copy = (*iter).first;
-    auto curr_coeff_copy = (*iter).second;
-    cout << "this curr (before calling thread): " << (*iter).second << "x^" << (*iter).first << endl;
-    threads.push_back(thread(multiply_term, curr_power_copy, curr_coeff_copy, other_copy, ref(temps.at(i)), k));
-    i++;
-  }
-
-  // call all threads
-  for (int i = 0; i < size; i++) {
-    threads.at(i);
+  auto this_end = (*this).poly.rend();
+  auto other_end = other.poly.rend();
+  for (auto this_iter = (*this).poly.rbegin(); this_iter != this_end; this_iter++) {
+    for (auto other_iter = other.poly.rbegin(); other_iter != other_end; other_iter++) {
+      power this_power = (*this_iter).first;
+      coeff this_coeff = (*this_iter).second;
+      power other_power = (*other_iter).first;
+      coeff other_coeff = (*other_iter).second;
+      // cout << "(before calling threads) this: " << this_coeff << "x^" << this_power << ", other: " << other_coeff << "x^" << other_power << endl;
+      threads.push_back(thread(multiply_terms, this_power, this_coeff, other_power, other_coeff, ref(temps.at(i))));
+      //threads.push_back(thread(multiply_terms, this_power, this_coeff, other_power, other_coeff, ref(result)));
+      threads.at(i);
+      i++;
+    }
   }
 
   // wait for threads to finish
@@ -300,11 +288,11 @@ polynomial polynomial::operator%(const polynomial& other) const {
     if (result.poly.empty() == true) {
       break;
     }
-    result.print();
   }
 
   return result;
 }
+
 // Test method to determine if coeff is correct at a specific power
 bool polynomial::check_coeff(power pwr, coeff exp_c) {
   // Check that 0 coefficient means that term is not in map
